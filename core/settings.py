2586ab289f99c3101fd82e2b198da469592a8f31
luismidv <perezmartluismiguel@gmail.com>
2025-01-29 09:57:42 +0100

    Commit on server

--- core/settings.py
+++ core/settings.py
@@ -59,7 +59,8 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://tfgserver.onrender.com"
 ]
 
 CSRF_COOKIE_NAME = 'csrftoken'
@@ -72,6 +73,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://tfgserver.onrender.com',
+
 ]
 
 CORS_ALLOW_CREDENTIALS = True
