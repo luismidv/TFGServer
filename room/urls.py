c049b8683f8ed8901a6da79383c129be5a2c7da9
luismidv <perezmartluismiguel@gmail.com>
2025-01-29 10:08:09 +0100

    Commit on server

--- room/urls.py
+++ room/urls.py
@@ -3,11 +3,12 @@ from django.views.generic import TemplateView
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from . import views
+from . import 
 app_name = 'room'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html")),
     path('api/my_endpoint/', views.my_api_view, name='my_api_view'),
-    path("csrf/", csrf_token_view),  # Fetch CSRF token
+    path("csrf/", views.csrf_token_view),  # Fetch CSRF token
 
 ]
