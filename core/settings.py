0bc06d4519d7df964a476111fbb25e08bc9d4d38
luismidv <perezmartluismiguel@gmail.com>
2025-02-18 10:58:22 +0100

    Commit om deploy

--- core/settings.py
+++ core/settings.py
@@ -64,6 +64,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://tfgserver.onrender.com"
+    "https://tfgluismi.netlify.app/"
 ]
 
 CSRF_COOKIE_NAME = 'csrftoken'
