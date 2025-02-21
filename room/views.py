a43c8a6ad0146813c7c958538a715ff310c2962a
luismidv <perezmartluismiguel@gmail.com>
2025-02-21 11:28:24 +0100

    first commit

--- room/views.py
+++ room/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.middleware.csrf import get_token
 import json
-
+my_email = "debanien@gmail.com"
 
 @csrf_exempt
 def csrf_token_view(request):
