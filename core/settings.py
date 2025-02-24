c34d67fc8b7a1a74ecf043206f4bc39f621b69f7
luismidv <perezmartluismiguel@gmail.com>
2025-02-24 13:29:33 +0100

    first commit

--- core/settings.py
+++ core/settings.py
@@ -65,7 +65,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://tfgserver.onrender.com",
-    "https://tfgluismi.netlify.app/",
+    "https://tfgluismi.netlify.app",
 
 ]
 
