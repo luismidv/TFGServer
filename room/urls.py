4421994da77429b5db086c54ccb313a4011e8ad5
luismidv <perezmartluismiguel@gmail.com>
2025-03-01 10:16:39 +0100

    New commit

--- room/urls.py
+++ room/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path("csrf/", views.csrf_token_view),  # Fetch CSRF token
     path('api/algorithm/', views.algo_view, name="algo_view"),
     path('accounts/' , include('django.contrib.auth.urls')),
-    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/user/', views.UserView.as_view(), name='user_view'),  
 
