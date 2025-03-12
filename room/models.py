1a11212e41df5ebe26601dc548d2d879566717d6
luismidv <perezmartluismiguel@gmail.com>
2025-03-12 17:15:13 +0100

    New

--- room/models.py
+++ room/models.py
@@ -5,42 +5,45 @@ from django.db import models
 #MODEL OF THE LESSOR TABLE IN THE KOYEB DB
 class Lessor(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.TextField(max_length=255)
-    telephone = models.TextField(max_length=9)
-    email = models.TextField(max_length=50)
-    room_id = models.ForeignKey('rooms', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    telephone = models.CharField(max_length=9)
+    email = models.CharField(max_length=50)
+    room_id = models.ForeignKey('Rooms', on_delete=models.CASCADE)
     
-    def _str_(self):
+    def __str__(self):
         return self.name
 
 #MODEL OF THE LESSOR TABLE IN THE KOYEB DB
 class Rooms(models.Model):
-    id = models.AutoField(primary_key=True)
-    direction = models.TextField(max_length=255)
-    city = models.TextField(max_length=50)
-    state = models.TextField(max_length=20)
-    rooms = models.TextField(max_length=100)
-    bathrooms = models.TextField(max_length=100)
-    metters = models.TextField(max_length=100)
-    price = models.TextField(max_length=100)
-    description = models.TextField(max_length=150)
+    
+    direction = models.CharField(max_length=255)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=20)
+    rooms = models.CharField(max_length=100)
+    bathrooms = models.CharField(max_length=100)
+    metters = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+    description = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.direction
 
 #MODEL OF THE TENANTS TABLE IN THE KOYEB DB
 class Tenants(models.Model):
     id = models.AutoField(primary_key=True)
-    names = models.TextField(max_length=255)
-    surnames = models.TextField(max_length=50)
-    age = models.TextField(max_length=20)
-    email = models.TextField(max_length=100)
-    worktimes = models.TextField(max_length=100)
-    schedules = models.TextField(max_length=100)
-    studies_level = models.TextField(max_length=100)
-    read = models.TextField(max_length=20)
-    pets = models.TextField(max_length=150)
-    cookies = models.TextField(max_length=100)
-    sport = models.TextField(max_length=100)
-    smoking = models.TextField(max_length=100)
-    organized = models.TextField(max_length=150)
+    names = models.CharField(max_length=255)
+    surnames = models.CharField(max_length=50)
+    age = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    worktimes = models.CharField(max_length=100)
+    schedules = models.CharField(max_length=100)
+    studies_level = models.CharField(max_length=100)
+    read = models.CharField(max_length=20)
+    pets = models.CharField(max_length=150)
+    cookies = models.CharField(max_length=100)
+    sport = models.CharField(max_length=100)
+    smoking = models.CharField(max_length=100)
+    organized = models.CharField(max_length=150)
 
     def __str__(self):
         return f"{self.names} {self.surnames}"
