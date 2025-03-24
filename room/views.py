6c0d9a7a2e58f1f83bfa7ddded984de2c5c2eb07
luismidv <perezmartluismiguel@gmail.com>
2025-03-24 11:37:37 +0100

    New

--- room/views.py
+++ room/views.py
@@ -123,7 +123,7 @@ def tenant_features(request):
                 else:
                     return 0
                 cursor.execute("""
-                    INSERT INTO tenants (id, names, surnames, age, email, worktime, biorythm, studies, read, pets, cook, sport, smoke, organized) 
+                    INSERT INTO tenants (id, Names, Surnames, Age, Email, Worktimes, Schedules, Studies level, Pets, Cooking, Sport, Smoking, Organized) 
                     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s)
                     """, [new_id,
                         data["names"], data["surnames"], data["age"],
