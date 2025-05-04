74384306bc7e10f3f8af1cf82fe45a7d4b192cb6
luismidv <perezmartluismiguel@gmail.com>
2025-05-04 11:57:39 +0200

    push

--- room/views.py
+++ room/views.py
@@ -227,8 +227,18 @@ def lessor_identification(request):
         elif type == "Refresh":
             with connection.cursor() as cursor:
                 username = data["username"]
+                cursor.execute("""
+                        SELECT id
+                        FROM auth_lessor 
+                        WHERE auth_lessor.username = %s
+                        """, [username])
+                result = cursor.fetchone()
                 lessor_information = get_rooms(username)
-                return JsonResponse({"message" : "Dara refreshed", "rooms_data" : lessor_information})
+                if len(lessor_information != 0):
+                    return JsonResponse({"message" : "Data refreshed != 0", "rooms_data" : lessor_information, "lessor_data" : result[0]})
+                else:
+                    return JsonResponse({"message" : "Data refreshed == 0", "rooms_data" : lessor_information, "lessor_data" : result[0]})
+                
         
 @csrf_exempt                
 def log_lessor(username, password):
@@ -254,6 +264,7 @@ def get_rooms(username):
             """, [username])
             columns = [col[0] for col in cursor.description]
             result = [dict(zip(columns, row)) for row in cursor.fetchall()]
+            return result
         except Exception as e:
             print(f"Error while trying to get user rooms:  {e}")
 
